34ccc1b328e4b0b3b2945deb8c97c8754204af26
Gabrielc30 <149747571+Gabrielc30@users.noreply.github.com>
2025-05-13 01:33:57 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,47 +38,7 @@ list4.addEventListener("click", () => {
 }*/
 
 // Seleccionar elementos del DOM
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-const list = document.querySelector("#list");
-const list2 = document.querySelector("#list2");
-const list3 = document.querySelector("#list3");
-const list4 = document.querySelector("#list4");
-const bodi = document.querySelector("#bodi");
 
-// Función para abrir el menú
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-    bodi.classList.add("visible");
-});
-
-// Función para cerrar el menú
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-    bodi.classList.remove("visible");
-});
-
-// Cerrar el menú al hacer clic en los elementos de navegación
-list.addEventListener("click", () => {
-    nav.classList.remove("visible");
-    bodi.classList.remove("visible");
-});
-
-list2.addEventListener("click", () => {
-    nav.classList.remove("visible");
-    bodi.classList.remove("visible");
-});
-
-list3.addEventListener("click", () => {
-    nav.classList.remove("visible");
-    bodi.classList.remove("visible");
-});
-
-list4.addEventListener("click", () => {
-    nav.classList.remove("visible");
-    bodi.classList.remove("visible");
-});
 document.addEventListener('DOMContentLoaded', function() {
     // Navigation menu for mobile
     const nav = document.querySelector("#nav");
